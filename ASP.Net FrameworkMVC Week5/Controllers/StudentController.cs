ea97193fb85d9ded15d0b04a161afd9ec973d434
navkhuoch21@outlook.com <navkhuoch21@outlook.com>
2022-01-12 21:55:23 +0700

    Add project files.

---
+++ ASP.Net FrameworkMVC Week5/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.Net_FrameworkMVC_Week5.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [ActionName("update")]
+        public ActionResult edit()
+        {
+            return View("pccfpi");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ASP.Net FrameworkMVC Week5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.Net_FrameworkMVC_Week5.MvcApplication" Language="C#" %>
